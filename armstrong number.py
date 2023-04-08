5cdb50d7cce6f6fee6cde38cf4dbe32d4c5d9971
Hrithik Gupta <hrithikguptagithub@gmail.com>
2023-04-08 16:56:55 +0530

    Fixing print statements

--- armstrong number.py
+++ armstrong number.py
@@ -1,10 +1,10 @@
-n=int(input("enter a number"))
+n=int(input("Enter a number : "))
 c,z=0,n
 while n !=0:
     d = n%10
     c = c + (d**3)
     n=n//10
 if c == z:
-    print("it is an armstrong number")
+    print("It is an armstrong number.")
 else:
-    print("it is not an armstrong number")
+    print("It is not an armstrong number.")
